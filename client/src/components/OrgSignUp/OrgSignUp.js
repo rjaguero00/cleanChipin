4dba901bec8547c51f40b9d795c5c308ef469faa
Grascon <gilberto.r.s@icloud.com>
2018-06-29 15:04:21 -0700

    Added darker background when modal pops up

--- client/src/components/OrgSignUp/OrgSignUp.js
+++ client/src/components/OrgSignUp/OrgSignUp.js
@@ -11,7 +11,15 @@ const customStyles = {
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
         width: '600px',
-        height: '400px'
+        height: '400px',
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)'
     }
 };
 
