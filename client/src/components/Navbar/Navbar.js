3a721197d7729641adebe7bb1ca4e578bb90ac76
Grascon <gilberto.r.s@icloud.com>
2018-06-29 14:07:53 -0700

    Changed navbar to fixed-top  and footer to sticky

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">
             Chipin
         </Link>
