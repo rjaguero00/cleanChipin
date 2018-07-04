51f85ebc0adafcbaff5c79dca15e02661d11f42d
Roberto aguero <rjaguero00@gmail.com>
2018-07-03 20:31:05 -0700

    Dashboard Routes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,21 +5,68 @@ import Eventcard from '../components/Eventcard/Eventcard';
 import SButton from '../components/SButton/SButton';
 
 class Dashboard extends Component {
+    state = {
+        currentPage: "/Dashboard",
+    };
+    componentDidMount() {
+        this.setState({ currentPage: this.props.location.pathname });
+    }
+    handlePageChange = page => {
+        this.setState({ currentPage: page });
+    };
 
+
+    //Remove an activity that a user no longer wishes to attend
+    notAttending = () => {
+
+    }
+
+    //Remove an activity that a user is no longer insteaded in
+    removeSavedActivity = () => {
+
+    }
+
+    //Mark saved activity to attending
+    atteningActivity = () => {
+
+    }
+
+
+
+    renderPage = () => {
+        if (this.state.currentPage === "/Dashboard") {
+            return (
+                <Wrapper>
+                    <Sidebar />
+                    <div>
+                        <SButton />
+                    </div>
+                    <div className="mx-auto">
+                        <Eventcard />
+                    </div>
+                </Wrapper>
+            );
+        } else if (this.state.currentPage === "/Dashboard/Saved") {
+            return (
+                <Wrapper>
+                    <Sidebar
+                        currentPage={this.state.currentPage}
+                    />
+                    Saved
+                    <div>
+                        <SButton />
+                    </div>
+                    <div className="mx-auto">
+                        <Eventcard />
+                    </div>
+                </Wrapper>
+            );
+        }
+    }
 
 
     render() {
-        return (
-            <Wrapper>
-                <Sidebar />
-                <div>
-                    <SButton />
-                </div>
-                <div className="mx-auto">
-                    <Eventcard />
-                </div>
-            </Wrapper>
-        );
+        return (<div>{this.renderPage()}</div>)
     };
 };
 
