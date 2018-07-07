5aeaacd4316ab048ff0afd95f99a6582e8ce905a
Grascon <gilberto.r.s@icloud.com>
2018-07-07 12:42:14 -0700

    Styling event cards, map modal, buttons, and logo

--- client/src/components/OrgSignUp/OrgSignUp.js
+++ client/src/components/OrgSignUp/OrgSignUp.js
@@ -86,12 +86,12 @@ class OrgSignUp extends React.Component {
                     <div>
                         <form>
                             <div className="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
+                                <label for="exampleInputEmail1" style={{color: 'white'}}>Email address</label>
                                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" input />
-                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <small id="emailHelp" className="form-text text-muted" style={{color: 'white'}}>We'll never share your email with anyone else.</small>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
+                                <label for="exampleInputPassword1" style={{color: 'white'}}>Password</label>
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" input />
                             </div>
                             <button
