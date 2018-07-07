ec7297f79682b36502469e60c867d12b39ee112b
EvelynCude <ebenitez@email.arizona.edu>
2018-07-07 13:52:43 -0700

    save route change

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,9 +7,9 @@ export default {
   },
 
   // For saving(favoriting) activiy
-  // saveActivity: function (activityData){
-  //   return axios.get("/api/activitiesRoutes/saveActivity", activityData);
-  // },
+  saveActivity: function (activityData){
+    return axios.get("/api/activitiesRoutes/saveActivity", activityData);
+  },
 
   // For saving(marking as attending) an activity
   saveAttending: function (attendingData) {
