1adaa2b5ba20d3d3e51173ed49e9aeb62f1d31e5
Roberto aguero <rjaguero00@gmail.com>
2018-07-10 20:58:00 -0700

    delete host working and rendering

--- client/src/components/HostEvents/Host/HostItem.js
+++ client/src/components/HostEvents/Host/HostItem.js
@@ -3,20 +3,16 @@ import API from '../../../utils/API.js';
 
 
 class HostItem extends Component {
-    state={
-        deleted: true
-    }
-
-    handleLoad = () => (
-        this.setState({deleted: true})
-    );
+    // handleLoad = () => (
+    //     this.props.loadActivities(this.props)
+    // );
     // Save a volunteer activity as one the user is attending
     deleteHostActivity = (event) => {
         event.preventDefault();
-        API.deleteHostActivity(this.props.id)
-            .then(this.handleLoad())
-            // .then(res => console.log("deleted"))
-            .catch(err => console.log(err));
+        API.deleteHostActivity(this.props.id
+        ).then(() => {
+            this.props.recollectData();
+        })
     }
     updateAllHours = (event) => {
         event.preventDefault();
@@ -33,8 +29,9 @@ class HostItem extends Component {
                     <h5 className="card-title ">
                         <a href="">{this.props.title}</a></h5>
                     <p className="card-text">Location: {this.props.location}</p>
-                    <button onClick={this.updateAllHours} className="btn btn-info">Verify All Hours</button>
-                    <button onClick={this.deleteHostActivity} className="btn btn-info">{this.loadActivities}Remove Post</button>
+                    <button onClick={this.updateAllHours} className="btn btn-primary">Verify All Hours</button>
+                    <button onClick={this.verify} className="btn btn-primary">Verify Hours</button>
+                    <button onClick={this.deleteHostActivity} className="btn btn-primary">Remove Post</button>
 
                     {/* <Modal
                         id={this.props.id}
